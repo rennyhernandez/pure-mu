7a1569b5cc1cd2fa76403b02706d1ab1b06fa4a5
Renny Hernandez <rennyhernand@gmail.com>
2014-07-26 14:27:17 -0430

    Added callback to retrieve public key

--- static/client-side/javascripts/controllers/app.js
+++ static/client-side/javascripts/controllers/app.js
@@ -4,14 +4,14 @@
         var env = this;
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         this.messages = [];
-        this.destinationPublicKey;
+        env.destinationPublicKey = null;
+        
         this.getUserInfo = function(){
                  $http.get('http://localhost:3000/api/user/fullinfo/' + $scope.to).success(function(data){                 
                  env.user = data
          });
         };
-        this.saveMessage = function(){
-        
+        this.saveMessage = function(){        
           this.user;
           this.message = { 
                             to: this.user.login,
@@ -20,12 +20,17 @@
                             createdAt: Date.now()
                           }
           
-          //Get destination public key
+          // Get destination public key
           $http.get('http://localhost:3000/api/publickey/' + this.user.login).success(function(data){
-            env.destinationPublicKey = data;                      
-          });
+            env.destinationPublicKey = data;     
+             
+          });                         
+          // Prepare Public key 
+//          var pkint = new forge.jsbn.BigInteger(env.destinationPublicKey 16);
+//          console.log(pkint);
           
-           //Save message to Server
+          
+           // Save message to Server
            $http.post('http://localhost:3000/api/message/send', this.message).
            success(function(data, status, headers, config) {
              env.messages.push(env.message);
