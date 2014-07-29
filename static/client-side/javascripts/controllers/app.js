0981bfc0441cac27eae416b8a0943d5d3c1ba879
Renny Hernandez <rennyhernand@gmail.com>
2014-07-29 15:34:23 -0430

    Generate BigInteger, create public key and encrypts message

--- static/client-side/javascripts/controllers/app.js
+++ static/client-side/javascripts/controllers/app.js
@@ -4,7 +4,9 @@
         var env = this;
         $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
         this.messages = [];
-        env.destinationPublicKey = null;
+        env.ciphertext;
+        env.destinationPublicKey; 
+        env.pkint;
         
         this.getUserInfo = function(){
                  $http.get('http://localhost:3000/api/user/fullinfo/' + $scope.to).success(function(data){                 
@@ -21,22 +23,31 @@
                           }
           
           // Get destination public key
-          $http.get('http://localhost:3000/api/publickey/' + this.user.login).success(function(data){
-            env.destinationPublicKey = data;     
-             
-          });                         
-          // Prepare Public key 
-//          var pkint = new forge.jsbn.BigInteger(env.destinationPublicKey 16);
-//          console.log(pkint);
-          
-          
-           // Save message to Server
-           $http.post('http://localhost:3000/api/message/send', this.message).
-           success(function(data, status, headers, config) {
-             env.messages.push(env.message);
-             $scope.body = $scope.to = null;
+          $http.get('http://localhost:3000/api/publickey/' + this.user.login).success(function(pkdata){          
 
-          });
+            //Retrieve public_n value, decode it from Base16 and create a new BigInt             
+            env.pkint = new forge.jsbn.BigInteger(pkdata.public_n, 16);
+            var public_e = pkdata.public_e.toString();
+            var exponent = new forge.jsbn.BigInteger(public_e,10);
+
+            console.log(exponent);
+            env.destinationPublicKey = forge.pki.setRsaPublicKey(env.pkint, exponent);
+            console.log(env.pkint.toString());
+            console.log(env.destinationPublicKey);
+            //encrypt message 
+            env.ciphertext = env.destinationPublicKey.encrypt(forge.util.createBuffer(env.message));
+            console.log(env.ciphertext);
+            // Save message to Server
+            $http.post('http://localhost:3000/api/message/send', this.message).
+            success(function(data, status, headers, config) {
+              env.messages.push(env.message);
+              $scope.body = $scope.to = null;
+            });
+          });                         
+
+          
+          
+
          
         };
         
