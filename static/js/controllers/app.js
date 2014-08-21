a4597c9301b967d8bff3103eebb9b0bd88cb294e
Renny Hernandez <rennyhernand@gmail.com>
2014-08-21 12:49:29 -0430

    Added Process message in server using REST API

--- static/js/controllers/app.js
+++ static/js/controllers/app.js
@@ -24,13 +24,13 @@
           envsave = this;       
           this.user;
           this.now = Date.now();
-          console.log(this.now);
           this.message = { 
                             to: env.user.login,
                             from: 'renny', //TODO: change value with username in session
                             body: $scope.body,                             
                             createdAt: this.now
                           }
+          this.toAuth;
 
           
           // Get destination public key
@@ -38,29 +38,35 @@
 
             //Retrieve public_n value, decode it from Base16 and create a new BigInt             
 
-            env.pkint = new forge.jsbn.BigInteger(public_n, 10);
+
             var public_e = pkdata.public_e.toString();
             var public_n = forge.util.decode64(pkdata.public_n);
-            
             var exponent = new forge.jsbn.BigInteger(public_e,10);
-
+            env.pkint = new forge.jsbn.BigInteger(public_n, 10);
+            //Recreate Public Key 
             env.destinationPublicKey = forge.pki.setRsaPublicKey(env.pkint, exponent);
 
-            //encrypt message 
-            env.ciphertext = env.destinationPublicKey.encrypt(forge.util.createBuffer(env.message));
-            console.log(env.ciphertext);
-            // Save message to Server        
-          this.datapacket = {
-                   to: env.user.login,
-                   from: 'renny', //TODO: change value with username in session
-                   body: env.ciphertext,
-                   createdAt: envsave.now
-          }
+            //encrypt message ... 
+            env.ciphertext = env.destinationPublicKey.encrypt(forge.util.createBuffer(env.message.body));
+            env.toAuth = env.ciphertext + "+" +  env.message.to + "+" + env.message.from + "+" + env.message.createdAt;
+            env.ciphertext64 = forge.util.encode64(env.ciphertext);
+            
+            //... then, sign. 
+            
+            // Save message
+            this.datapacket = {
+                     to: env.user.login,
+                     from: 'renny', //TODO: change value with username in session
+                     body: env.ciphertext64,
+                     createdAt: env.message.createdAt,
+                     authMessage: env.toAuth //TODO: Authenticate this string
+            };
+            
             $http.post('http://localhost:3000/api/message/send', this.datapacket).
             success(function(data, status, headers, config) {
-              console.log("hello, here");
               env.messages.push(env.message);
               $scope.body = $scope.to = null;
+              env.foundUser = false;
             });
           });                                 
         };
